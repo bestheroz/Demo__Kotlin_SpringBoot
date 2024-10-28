f592c7bd93579f9f60a2b25dfeff071b37d628e5
joony.kim <bestheroz@gmail.com>
2024-10-28 13:51:47 +0900

    ⬆️update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.3")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.4")
         mavenBom("io.awspring.cloud:spring-cloud-aws-dependencies:3.2.0-M1")
     }
 }
